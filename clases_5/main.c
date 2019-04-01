ff60b7469593a54905de6666f618278474919acf
nicoocampo <nicolasocampo94@gmail.com>
2019-04-01 10:07:45 -0300

    hretjrrae hgfhjfgbg

--- clases_5/main.c
+++ clases_5/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 3
 
+void cargarDatos(int numeros[], int cantidadDeValores);
+void mostrarDatos(int [], int);
 int main()
 {
-    printf("Hello world!\n");
+    int numeros[MAX];
+    cargarDatos(numeros, MAX);
+    mostrarDatos(numeros, MAX);
     return 0;
+}
+void cargarDatos(int numeros[], int cantidadDeValores){
+    int i;
+    for(i=0;i<cantidadDeValores; i++){
+        printf("\nIngrese el valor %d: ", i+1);
+        scanf("%d", &numeros[i]);
+    }
+}
+void mostrarDatos(int numeros[], int cantidadDeValores){
+    int i;
+    for(i=0;i<cantidadDeValores; i++){
+        printf("\n\nEl valor del espacio %d es: %d", i+1, numeros[i]);
+    }
 }
