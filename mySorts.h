cb351e28913d273f776ea0b890dc05758ee69a51
Nicolás Ocampo <nicolas.ocampo@hotmail.com>
2019-04-30 14:53:29 -0300

    Add files via upload

---
+++ mySorts.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<ctype.h>
+
+void burbujeoInt(int enteros[],int len);
+void burbujeoFloat(float flotante[],int len);
+void burbujeoString(char cadena[][20],int len);
+void swapBubbleInt(int enteros[], int i, int j);
+void swapBubbleChar(char caracteres[], int i, int j);
+void swapBubbleFloat(float flotantes[], int i, int j);
+void swapBubbleString(char cadena[], int i, int j);
