cc29e07e1f0399e07242c1119733205d64b5eda8
Nico <nicolasocampo94@gmail.com>
2019-05-14 11:24:55 -0300

    copia de punteros

--- clase 15/main.c
+++ clase 15/main.c
@@ -8,13 +8,21 @@ int main(){
     x=&v;//LE PASAMOS LA POSICION DE MEMORIA A LA CUAL TIENE QUE APUNTAR EL PUNTERO
     printf("Valor de v: %d\n",*x);//MOSTRAMOS EL VALOR DE LA VARIABLE v DESDE EL PUNTERO x
     printf("Posicion de memoria de v: %p\n\n",x);//MOSTRAMOS LA DIRECCION DE MEMORIA QUE GUARDO EL PUNTERO
-    return 0;
-    */
-    char* pLetra;
+    return 0;*/
+
+    /**char* pLetra=NULL;
     char letra;
-    //pLetra=&letra;
+    pLetra=&letra;
     printf("Ingrese una letra: ");
     scanf("%c",pLetra);
     printf("Valor de la variable letra: %c\n",*pLetra);
+    return 0;*/
+    int p;
+    int* q;
+    int* r;
+    p=10;
+    q=&p;
+    r=q;
+    printf("p= %d\nq= %d\nr= %d", p,*q,*r);
     return 0;
 }
